eb5aaee73e72b638a0695f3c80d5fe982c9bc670
Ruffa <ruffaresentes@me.com>
2025-03-28 10:05:11 +0800

    initial commit

---
+++ src/main/java/copro2/w10_gui/W10_GUI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package copro2.w10_gui;
+
+/**
+ *
+ * @author RS
+ */
+public class W10_GUI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
