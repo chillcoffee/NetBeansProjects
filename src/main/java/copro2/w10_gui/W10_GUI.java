32dd90b39c3f83ea19ad1e7a016824be27446ca8
Ruffa <ruffaresentes@me.com>
2025-03-28 10:23:28 +0800

    edit on browser github

--- src/main/java/copro2/w10_gui/W10_GUI.java
+++ src/main/java/copro2/w10_gui/W10_GUI.java
@@ -1,16 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package copro2.w10_gui;
 
-/**
- *
- * @author RS
- */
+
 public class W10_GUI {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!"); 
+        //edited in github browser chrome
     }
 }
